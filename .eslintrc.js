57246913420969e2c9e9521a4d00d62fa90aea35
GilbertoSVN <nvotreblig@gmail.com>
2020-03-03 22:40:49 -0300

    added react hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
@@ -33,5 +33,7 @@ module.exports = {
         allow: ['tron'],
       },
     ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
